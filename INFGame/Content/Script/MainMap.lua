d9e1d75abe16268df84703a7dcc5822ba6643f5b
Caint0w0 <2393649488@qq.com>
2025-10-15 23:51:32 +0800

    Init

---
+++ INFGame/Content/Script/MainMap.lua
@@ -0,0 +1,38 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+
+---@type MainMap_C
+local M = UnLua.Class()
+
+-- function M:Initialize(Initializer)
+-- end
+
+-- function M:UserConstructionScript()
+-- end
+
+function M:ReceiveBeginPlay()
+    UE4.UKismetSystemLibrary.PrintString(self, "Game BeginPlay")
+end
+
+-- function M:ReceiveEndPlay()
+-- end
+
+function M:ReceiveTick(DeltaSeconds)
+    UE4.UKismetSystemLibrary.PrintString(self, "1111")
+end
+
+-- function M:ReceiveAnyDamage(Damage, DamageType, InstigatedBy, DamageCauser)
+-- end
+
+-- function M:ReceiveActorBeginOverlap(OtherActor)
+-- end
+
+-- function M:ReceiveActorEndOverlap(OtherActor)
+-- end
+
+return M
